c0086da3b10fdbad95d12cbd4d0411313ba312d6
Raúl Pérez <jrpcassone@gmail.com>
2023-08-14 23:33:55 +0200

    Add movie counter and his render test

--- src/components/Count/Count.tsx
+++ src/components/Count/Count.tsx
@@ -1,5 +1,14 @@
+import { useContext } from "react";
+import FilmsContext from "../../store/films/FilmsContext";
+
 const Count = (): React.ReactElement => {
-  return <span className="count">Listando 4 películas</span>;
+  const { films } = useContext(FilmsContext);
+
+  return (
+    <span aria-label="shown movies" className="count">
+      Listando {films.length} películas
+    </span>
+  );
 };
 
 export default Count;
